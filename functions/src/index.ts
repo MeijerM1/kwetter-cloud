eaa5853971aa229784eb62ac0eaf9f757f903fe0
MeijerM1 <max1_meijer@hotmail.com>
2019-06-06 16:00:10 +0200

    Add favorite getting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -16,7 +16,7 @@ export const addFavorite = functions.https.onRequest((request, response) => {
         const userTweet: UserTweet = new UserTweet();
         userTweet.user = request.body.user;
         userTweet.tweet = request.body.tweet;
-    
+
         if (!userTweet.isValid()) {
             response.status(400).send('invalid body')
         }
@@ -31,3 +31,43 @@ export const addFavorite = functions.https.onRequest((request, response) => {
 
     }
 });
+
+export const favorites = functions.https.onRequest((request, response) => {
+
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Headers', request.header('Access-Control-Request-Headers'));
+    response.header('Access-Control-Allow-Methods', request.header('Access-Control-Request-Method'));
+
+    console.log(request.method);
+
+    if (request.method === 'OPTIONS') {
+        response.send(204);
+    } else if (request.method === 'GET') {
+        const user: string = request.query.user;
+
+
+        if (!user) {
+            response.status(400).send('No user specified in query params');
+        }
+
+        const query: any = firebase.firestore().collection('favorites').where('user', '==', user);
+
+        query.get()
+            .then((snapshot: any) => {
+                if (!snapshot.empty) {
+                    const tweets: any = [];
+                    snapshot.docs.forEach((el: any) => {
+                        const userTweet = el.data();
+                        tweets.push(userTweet.tweet)
+                    });
+                    response.status(200).send(tweets);
+                } else {
+                    response.status(204).send();
+                }
+
+            }).catch((err: any) => {
+                response.status(400).send(err);
+            })
+    }
+});
+
